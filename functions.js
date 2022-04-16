4515f2b7860ac8c3c807787cb5613081f729c28d
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-04-15 20:38:47 -0400

    Add functions

---
+++ functions.js
@@ -0,0 +1,16 @@
+// Funcion declarativa
+function saludar() {
+  console.log('Hola mundo');
+}
+
+// Expresiones funcionales
+var saludo = function () {
+  console.log('Hola mundo');
+};
+
+// Funciones flechas
+const saludando = () => {
+  console.log('Hola mundo');
+};
+
+saludando();
