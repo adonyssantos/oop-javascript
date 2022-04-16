cfab604f429485b26ec95097c7d33e6e54812ce2
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-04-15 21:13:58 -0400

    Add video player

--- src/classes/CourseClass.mjs
+++ src/classes/CourseClass.mjs
@@ -1,7 +1,26 @@
+const videoPlay = videoId => {
+  const videoUrl = `https://www.adonys.me/class/${videoId}`;
+  console.log(`videoPlay: ${videoUrl}`);
+};
+
+const videoStop = videoId => {
+  const videoUrl = `https://www.adonys.me/class/${videoId}`;
+  console.log(`videoStop: ${videoUrl}`);
+};
+
 class CourseClass {
-  constructor({ id, title }) {
+  constructor({ id, title, videoId }) {
     this.id = id;
     this.title = title;
+    this.videoId = videoId;
+  }
+
+  play() {
+    videoPlay(this.videoId);
+  }
+
+  stop() {
+    videoStop(this.videoId);
   }
 }
 
